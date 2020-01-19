bdbb29933e7b6ec2fd1266b7b41b8856fd74af77
Prakash Chandra <prakash.jat@xebia.com>
2020-01-19 22:56:12 +0530

    class in progress

--- class.js
+++ class.js
@@ -28,7 +28,7 @@ function myFunction(name, surname) {
     this.surname = surname;
 }
 
-myFunction.prototype.display = function() {
+myFunction.prototype.display = function () {
     return `Hi this is ${this.name}`;
 }
 
@@ -44,12 +44,12 @@ let Test = class {
 
 let User = class MyClass {
     sayHi() {
-      console.log(MyClass); // MyClass name is visible only inside the class
+        console.log(MyClass); // MyClass name is visible only inside the class
     }
-  };
-  
-  new User().sayHi(); // works, shows MyClass definition
-  
+};
+
+new User().sayHi(); // works, shows MyClass definition
+
 //console.log(MyClass);
 
 function testCall(value) {
@@ -65,22 +65,22 @@ new test().display();
 
 /* Getters and Setters */
 class newTest {
-     constructor(val) {
-        this.val = val;
-        console.log('val',val)
-    } 
+    constructor(name) {
+        this.name = name;
+        console.log('val', this.name)
+    }
 
     get name() {
-        return this.name;
+        return this._name;
     }
 
     set name(name) {
         if (name.length < 4) {
             console.log("Name is too short.");
             return;
-          }
-          this.name = value;
-          console.log("Your name is", this.name);
+        }
+        this._name = name;
+        console.log("Your name is", this._name);
     }
 }
 
@@ -88,5 +88,104 @@ let ntest = new newTest("sachin");
 console.log(ntest);
 
 
+/* Private and Protected Properties and method */
+class CoffeeMachine {
+    _waterAmount = 0;
 
+    set waterAmount(value) {
+        if (value < 0) throw new Error("Negative water");
+        this._waterAmount = value;
+    }
 
+    get waterAmount() {
+        return this._waterAmount;
+    }
+
+    constructor(power) {
+        // this._power = power;
+    }
+
+}
+
+// create the coffee machine
+let coffeeMachine = new CoffeeMachine(100);
+
+// add water
+//coffeeMachine.waterAmount = -10;
+
+/* Private variable */
+class varClass {
+    _proVar;
+
+    setProVar(val) {
+        this._proVar = val;
+    }
+
+    getProVar() {
+        return this._proVar;
+    }
+
+}
+
+let varObj = new varClass();
+varObj.setProVar(100);
+console.log(varObj.getProVar());
+console.log(varObj._proVar);
+console.log(varObj instanceof varClass);
+
+/* Inheritance */
+
+class Animal {
+    name = 'Rabit';
+    speed = '5';
+
+    constructor(...arr) {
+        console.log('calling animal constructor');
+        console.log(arr);
+        this.name = arr[0];
+        this.speed = arr[1];
+    }
+}
+
+// super is compulsory to call when extending the class
+
+class Rabit extends Animal {
+    constructor(...args) {
+        console.log('calling rabit constructor');
+        super(...args);
+    }
+    display() {
+        return `This is ${this.name} and speed is ${this.speed}`
+    }
+}
+
+new Rabit('Rat', '5');
+//new Animal('Parrot', '10')
+
+//console.log(new Rabit('Rat', '5').display());
+//console.log(new Rabit() instanceof Animal);
+
+function f() {
+    return class {
+        name = 'sachin';
+        city = 'sillod';
+
+        display() {
+            console.log("from return class")
+            return `This is ${this.name} from return ${this.city}`;
+        }
+
+    }
+}
+
+class testFunction extends f() {
+    display() {
+        //setInterval(()=>super.display(),1000)    
+        super.display();
+        console.log("from testFunction");
+        return `This is ${this.name} from testFunction ${this.city}`;
+    }
+}
+
+var fClass = f();
+console.log(new testFunction().display());
